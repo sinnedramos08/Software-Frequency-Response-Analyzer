2727b415798c6c4952526f61224d77d987ecc513
Dennis Andrew Ramos <sinnedramos.org01@gmail.com>
2026-07-01 21:46:46 +0800

    Added new coefficient tests for iloop

--- Python Plotter/plotter_bode.py
+++ Python Plotter/plotter_bode.py
@@ -9,9 +9,9 @@ import matplotlib.pyplot as plt
 import serial
 
 # Configuration
-SERIAL_PORT = "COM10"          # Change this to your serial port
+SERIAL_PORT = "COM11"          # Change this to your serial port
 SERIAL_BAUD = 115200           # Change this if your UART uses another baud rate
-COMPARE_CSV_PATH =None#"DATA\\SIM_ILOOP_CORRECTED.csv"         # Set to a compare CSV path, or leave empty to disable
+COMPARE_CSV_PATH =None #"SIM_ILOOP_FX_5KHZ_PM_63DEG_GM_14DB.csv"         # Set to a compare CSV path, or leave empty to disable
 SAVE_FILENAME = "sweep_data.csv"  # Default file name for UART sweep save
 
 
