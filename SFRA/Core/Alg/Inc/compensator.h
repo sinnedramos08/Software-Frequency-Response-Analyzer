b7f48a38daf6a465807c3ead3b796ced6f2b3630
Dennis Andrew Ramos <sinnedramos.org01@gmail.com>
2026-07-23 14:14:11 +0800

    Added Digital Filter struct, init, and update

--- SFRA/Core/Alg/Inc/compensator.h
+++ SFRA/Core/Alg/Inc/compensator.h
@@ -33,6 +33,10 @@
 #define A1_V (+1.9800607277045987)
 #define A2_V (-0.9800607277045986)
 
+#define FILTER_B0 (0.1257621926f)
+#define FILTER_B1 (0.1257621926f)
+#define FILTER_A1 (0.7484756149f)
+
 #define KP	(78.53f)
 #define KI	(1.0447202192208f)
 
@@ -87,10 +91,21 @@ typedef struct
 	float		f_out;
 }discrete_pi_controller_t;
 
+typedef struct
+{
+    float f_ref;
+    float f_a1;
+    float f_b0;
+    float f_b1;
+    float f_x[2];
+    float f_y[2];
+    float f_out;
+} filter_LPFData_t;
 
 extern discrete_pi_controller_t	discretepi_iloop;
 extern compensator_2p2z_t 		comp2p2z_iloop;
 extern compensator_2p2z_t		comp2p2z_vloop;
+extern filter_LPFData_t			Order1_LPF_filter;
 
 //Function prototypes
 void compensator_2P2Z_Init(compensator_2p2z_t * p_compensator, float f_ref, float f_a1, float f_a2, float f_b0, float f_b1, float f_b2, float f_k);
@@ -99,8 +114,43 @@ inline static void compensator_2P2Z_Update(compensator_2p2z_t * p_compensator);
 void pi_Discrete_Controller_Init(discrete_pi_controller_t * p_pi_controller, float f_kp, float f_ki);
 static inline void pi_Discrete_Controller_Update(discrete_pi_controller_t * p_pi_controller);
 
+void filter_Order1_LPF_Init(filter_LPFData_t * p_filter,float f_a1, float f_b0, float f_b1);
+inline static void filter_Order1_LPF_Update(filter_LPFData_t * p_filter);
 
 //__attribute__( ( section ( ".ccmram" ) ) )
+inline static void filter_Order1_LPF_Update(filter_LPFData_t * p_filter)
+{
+	/*
+	 * x0 = x[n] -> Current Input
+	 * x1 = x[n-1] -> Previous Input
+	 * y1 = y[n-1] -> Previous Output
+	 */
+
+	// Save Inputs:
+    const float x0 = p_filter->f_ref;	// Current Input
+    const float x1 = p_filter->f_x[0];	// Previous Input
+    const float y1 = p_filter->f_y[0];	// Previous Output
+
+    /*
+     * Difference Equation:
+	 * y[n] = -a1*y[n-1]+b0*x[n]+b1*x[n-1]
+	 * From tool
+	 * y[n] = a1*y[n-1]+b0*x[n]+b1*x[n-1]
+	 * Defined value for a1 is negated from original
+	 */
+    const float y0 = (p_filter->f_a1 * y1) + (p_filter->f_b0 * x0) + (p_filter->f_b1 * x1);	// Compute Current Output
+
+    // Save and Shift Inputs and Outputs
+    p_filter->f_x[1] = x1;	// Second Old Value, ignored
+    p_filter->f_x[0] = x0;	// Put Current Input to x[0] to save as previous
+    p_filter->f_y[1] = y1;	// Second Old Value, ignored
+    p_filter->f_y[0] = y0;	// Put current output to y[0] to save as previous
+
+    // Output y0
+    p_filter->f_out= y0;	// Current Output
+
+}
+
 inline static void compensator_2P2Z_Update(compensator_2p2z_t * p_compensator)
 {
 	float acc;
--- SFRA/Core/Alg/Src/compensator.c
+++ SFRA/Core/Alg/Src/compensator.c
@@ -9,6 +9,22 @@
 discrete_pi_controller_t	discretepi_iloop;
 compensator_2p2z_t 			comp2p2z_iloop;
 compensator_2p2z_t			comp2p2z_vloop;
+filter_LPFData_t			Order1_LPF_filter;
+
+void filter_Order1_LPF_Init(filter_LPFData_t * p_filter,float f_a1, float f_b0, float f_b1)
+{
+	memset(p_filter, 0, sizeof(*p_filter));
+	/*
+	 * Difference Equation:
+	 * y[n] = -a1*y[n-1]+b0*x[n]+b1*x[n-1]
+	 * From tool
+	 * y[n] = a1*y[n-1]+b0*x[n]+b1*x[n-1]
+	 * Defined value for a1 is negated from original
+	 */
+    p_filter->f_a1 = f_a1;
+    p_filter->f_b0 = f_b0;
+    p_filter->f_b1 = f_b1;
+}
 
 void compensator_2P2Z_Init(compensator_2p2z_t * p_compensator, float f_ref, float f_a1, float f_a2, float f_b0, float f_b1, float f_b2, float f_k)
 {
